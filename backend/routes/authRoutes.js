4a9fc795fe6d0d69f193e956af4644f7819ec475
DeliceLydia <Delydia84@gmail.com>
2023-09-04 16:34:57 +0200

    ft: login a user, get access and refresh a token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import registerUser from "../controlllers/auth/registerController.js";
 import verifyUserEmail from "../controlllers/auth/verifyEmailController.js";
-
+import loginUser from "../controlllers/auth/loginController.js";
+import {loginLimiter} from '../middleware/apiLimiter.js'
+ 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
+router.post("/login", loginLimiter, loginUser)
 
 export default router;
